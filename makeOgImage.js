9e9e0353da050b3351f48bdec1f462081244a9a2
Vitaliy Volynskiy <i@vitalya.me>
2020-10-29 12:01:51 +0600

    Зум для og:image

--- makeOgImage.js
+++ makeOgImage.js
@@ -7,6 +7,7 @@ async function run () {
   await page.setViewport({ width: 1200, height: 630 })
   const html = fs.readFileSync('./_site/index.html', 'utf8')
   await page.setContent(html)
+  await page.evaluate(() => { document.body.style.zoom = 1.6 })
   await page.screenshot({ path: '/tmp/og-image.png' })
   await browser.close()
 }
