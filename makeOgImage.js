752aa17356e76733d708615c1f2a8d6c27ebe191
Vitaliy Volynskiy <i@vitalya.me>
2020-10-29 10:32:09 +0600

    OpenGraph image и скрипт для ее генерации

---
+++ makeOgImage.js
@@ -0,0 +1,14 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs')
+
+async function run () {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1200, height: 630 })
+  const html = fs.readFileSync('./_site/index.html', 'utf8')
+  await page.setContent(html)
+  await page.screenshot({ path: '/tmp/og-image.png' })
+  await browser.close()
+}
+
+run()
