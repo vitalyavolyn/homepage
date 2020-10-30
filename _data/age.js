a39b0edbd91a631e718b4633432db9daedef8398
Vitaliy Volynskiy <i@vitalya.me>
2020-10-30 11:14:26 +0600

    Фикс возраста
    Все таки не 1 января родился
    Гений мысли
    Отец расчета времени

--- _data/age.js
+++ _data/age.js
@@ -1,1 +1,4 @@
-module.exports = new Date().getFullYear() - 2001
+module.exports = () => {
+  const ageDifMs = Date.now() - new Date('2001-02-25')
+  return new Date(ageDifMs).getUTCFullYear() - 1970
+}
