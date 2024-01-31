afed950f8e4d201d6b15a45ab1c2c45b2a6d5112
Vitaliy Volynskiy <i@vitalya.me>
2024-01-31 15:59:24 +0600

    Hide some links

--- _data/links.js
+++ _data/links.js
@@ -1,10 +1,10 @@
 const username = 'vitalyavolyn'
 
 const links = {
-  'ВКонтакте': `https://vk.com/${username}`,
+  // 'ВКонтакте': `https://vk.com/${username}`,
   'Telegram': `https://t.me/${username}`,
   'GitHub': `https://github.com/${username}`,
-  'Keybase': `https://keybase.io/${username}`,
+  // 'Keybase': `https://keybase.io/${username}`,
   'i@vitalya.me': `mailto:i@vitalya.me`
 }
 
